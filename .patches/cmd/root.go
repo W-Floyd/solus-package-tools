2bacb72d34d63234ef858e30ffe05b033e3ebdba
William Floyd <william.png2000@gmail.com>
2019-07-30 21:23:45 -0400

    root: Remove config stuff, we don't need it (yet?)

--- .patches/cmd/root.go
+++ .patches/cmd/root.go
@@ -1,6 +1,6 @@
 ---
 +++
-@@ -1,103 +1,64 @@
+@@ -1,103 +1,28 @@
 -/*
 -Copyright © 2019 William Floyd <william.png2000@gmail.com>
 -
@@ -28,20 +28,18 @@
 -  "fmt"
 -  "os"
 -  "github.com/spf13/cobra"
+-
+-  homedir "github.com/mitchellh/go-homedir"
+-  "github.com/spf13/viper"
 +	"fmt"
 +	"os"
  
--  homedir "github.com/mitchellh/go-homedir"
--  "github.com/spf13/viper"
 +	"github.com/spf13/cobra"
- 
-+	homedir "github.com/mitchellh/go-homedir"
-+	"github.com/spf13/viper"
  )
  
 -
- var cfgFile string
- 
+-var cfgFile string
+-
 -
 -// rootCmd represents the base command when called without any subcommands
  var rootCmd = &cobra.Command{
@@ -78,25 +76,22 @@
  
  func init() {
 -  cobra.OnInitialize(initConfig)
-+	cobra.OnInitialize(initConfig)
- 
+-
 -  // Here you will define your flags and configuration settings.
 -  // Cobra supports persistent flags, which, if defined here,
 -  // will be global for your application.
-+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.solus-package-util.yaml)")
- 
+-
 -  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.solus-package-util.yaml)")
 -
 -
 -  // Cobra also supports local flags, which will only run
 -  // when this action is called directly.
 -  rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
  }
- 
 -
- // initConfig reads in config file and ENV variables if set.
- func initConfig() {
+-
+-// initConfig reads in config file and ENV variables if set.
+-func initConfig() {
 -  if cfgFile != "" {
 -    // Use config file from the flag.
 -    viper.SetConfigFile(cfgFile)
@@ -119,27 +114,5 @@
 -  if err := viper.ReadInConfig(); err == nil {
 -    fmt.Println("Using config file:", viper.ConfigFileUsed())
 -  }
-+	if cfgFile != "" {
-+		// Use config file from the flag.
-+		viper.SetConfigFile(cfgFile)
-+	} else {
-+		// Find home directory.
-+		home, err := homedir.Dir()
-+		if err != nil {
-+			fmt.Println(err)
-+			os.Exit(1)
-+		}
-+
-+		// Search config in home directory with name ".solus-package-util" (without extension).
-+		viper.AddConfigPath(home)
-+		viper.SetConfigName(".solus-package-util")
-+	}
-+
-+	viper.AutomaticEnv() // read in environment variables that match
-+
-+	// If a config file is found, read it in.
-+	if err := viper.ReadInConfig(); err == nil {
-+		fmt.Println("Using config file:", viper.ConfigFileUsed())
-+	}
- }
+-}
 -
