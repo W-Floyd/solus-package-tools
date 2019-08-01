38e21eee36c804031f0733ede102b4f27f248be0
William Floyd <william.png2000@gmail.com>
2019-07-31 22:04:47 -0400

    some changes, helps building, still got a nasty infinite loop somewhere tho...

--- .patches/cmd/build/build.go
+++ .patches/cmd/build/build.go
@@ -1,10 +1,11 @@
 ---
 +++
-@@ -0,0 +1,75 @@
+@@ -0,0 +1,86 @@
 +package build
 +
 +import (
 +	"fmt"
++	"log"
 +
 +	"github.com/W-Floyd/solus-package-tools/solus-package-util/cmd/packages"
 +)
@@ -18,7 +19,11 @@
 +
 +		newQueue := []string{}
 +
-+		for _, targetPackage := range *buildQueue {
++		for _, realPackage := range *buildQueue {
++
++			targetPackage := packages.TestTrimmedName(realPackage)
++
++			globalState = packages.GetState()
 +
 +			if globalState[targetPackage].Built || packages.IsPackageFailed(targetPackage, &globalState) {
 +				continue
@@ -29,31 +34,37 @@
 +				newQueue = append(newQueue, packages.RundepRecurse(builddep, &globalState)...)
 +			}
 +
++			// Keep in queue for now, to hopefully build later.
 +			if !packages.IsPackageBuildable(targetPackage, &globalState) {
 +				newQueue = append(newQueue, targetPackage)
-+				//fmt.Println(targetPackage + " is unbuildable.")
 +				continue
 +			}
 +
++			log.Println("Attributes of " + targetPackage)
++			log.Println(globalState[targetPackage].Attributes.Builddeps)
++
 +			buildPrepare(targetPackage, &globalState)
 +			solbuildOffload(targetPackage, &globalState)
-+
-+			globalState = packages.GetState()
 +
 +		}
 +
 +		*buildQueue = newQueue
 +
++		if len(*buildQueue) == 0 {
++			continue
++		}
++
 +		if !checkBuildQueueForBuildability(*buildQueue, &globalState) {
 +			fmt.Println("Queue is unbuildable")
-+			/* 			for _, dep := range globalState {
-+				json, _ := json.Marshal(dep)
-+				fmt.Println(string(json))
-+			} */
++			for _, dep := range *buildQueue {
++				log.Println(dep)
++			}
 +			break
 +		}
 +
 +	}
++
++	log.Println("Done Building")
 +
 +	packages.WritePackageCache(&packages.PackageCacheStore)
 +
