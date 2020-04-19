c3c9f829e65b077cc0d805ae014a8f38d3a2aa8b
William Floyd <william.png2000@gmail.com>
2020-04-19 15:08:35 -0400

    init: Format, simplify cobra calling.

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 while read -r __program; do
-    if ! which "${__program}" &> /dev/null; then
+    if ! which "${__program}" &>/dev/null; then
         echo "Please install ${__program}"
         exit 1
     fi
-done <<< "cobra
+done <<<"cobra
 patch
 diff"
 
@@ -19,6 +19,10 @@ __package_full="${__package_path}/${__package_name}"
 __package_author="William Floyd <william.png2000@gmail.com>"
 __package_license="MIT"
 
+__cobra() {
+    cobra -a "${__package_author}" -l "${__package_license}" ${@}
+}
+
 if [ -d "${__package_name}" ] && [ -d ".${__package_name}" ]; then
 
     if [ -d '.patches' ]; then
@@ -27,17 +31,17 @@ if [ -d "${__package_name}" ] && [ -d ".${__package_name}" ]; then
 
     mkdir '.patches'
 
-    pushd "${__package_name}" 1> /dev/null
+    pushd "${__package_name}" 1>/dev/null
 
     find . -type f | while read -r __file; do
         __patch="$(diff -Nau "../.${__package_name}/${__file}" "${__file}" | sed -e '1,2s/ .*//')"
         if ! [ -z "${__patch}" ]; then
             mkdir -p "$(dirname "../.patches/${__file}")"
-            echo "${__patch}" > "../.patches/${__file}"
+            echo "${__patch}" >"../.patches/${__file}"
         fi
     done
 
-    popd 1> /dev/null
+    popd 1>/dev/null
 
     rm -r "${__package_name}"
     rm -r ".${__package_name}"
@@ -49,28 +53,24 @@ fi
 
 ################################################################################
 
-pushd "${__package_name}" 1> /dev/null
+pushd "${__package_name}" 1>/dev/null
 
 {
 
-    cobra -a "${__package_author}" -l "${__package_license}" init --pkg-name "${__package_full}"
+    __cobra init --pkg-name "${__package_full}"
 
     while read -r __command; do
-        cobra add "${__command}" -a "${__package_author}" -l "${__package_license}"
-    done <<< 'build
+        __cobra -l "${__package_license}"
+    done <<<'build
 git
 list
 graph
 info
 bootstrap'
 
-    cobra -a "${__package_author}" -l "${__package_license}" add -p gitCmd bump
-    cobra -a "${__package_author}" -l "${__package_license}" add -p gitCmd upgrade
-    cobra -a "${__package_author}" -l "${__package_license}" add -p gitCmd rebuild
+} 1>/dev/null
 
-} 1> /dev/null
-
-popd 1> /dev/null
+popd 1>/dev/null
 
 ################################################################################
 
@@ -78,7 +78,7 @@ cp -r "${__package_name}" ".${__package_name}"
 
 if [ -d '.patches' ]; then
 
-    pushd '.patches' 1> /dev/null
+    pushd '.patches' 1>/dev/null
 
     find . -type f | while read -r __file; do
 
@@ -88,19 +88,19 @@ if [ -d '.patches' ]; then
 
         touch "../${__package_name}/${__file}"
 
-        patch --ignore-whitespace "../${__package_name}/${__file}" "${__file}" 1> /dev/null
+        patch --ignore-whitespace "../${__package_name}/${__file}" "${__file}" 1>/dev/null
     done
 
-    popd 1> /dev/null
+    popd 1>/dev/null
 
 fi
 
 ################################################################################
 
-pushd "${__package_name}" 1> /dev/null
+pushd "${__package_name}" 1>/dev/null
 
 go build -o ~/go/bin/${__package_name}
 
-popd 1> /dev/null
+popd 1>/dev/null
 
 exit
